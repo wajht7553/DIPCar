c9b3e330aaa8cd0490c8926f7ac6c2962967b10b
wajht7553 <wajahatullah235@gmail.com>
2024-09-03 12:23:45 +0500

    Code object detection

--- src/perception/object_detection.py
+++ src/perception/object_detection.py
@@ -1,36 +1,24 @@
-import cv2
-import torch
-from src.perception.utils.camera import Camera
-from src.perception.utils.visualizations import draw_detections
+#!/usr/bin/python3
+from jetson.inference import detectNet
+from jetson.utils import videoSource, videoOutput
 
 
-class ObjectDetector:
-    def __init__(self, model_path='../../data/models/yolov5s.pt'):
-        self.model = torch.hub.load('ultralytics/yolov5',
-                                    'custom', path=model_path)
+net = detectNet(
+    model='../../data/models/ssd-mobilenet.onnx',
+    threshold=0.5
+    )
 
-    def detect_objects(self, frame):
-        results = self.model(frame)
-        detections = []
-        for *box, conf, cls in results.xyxy[0]:
-            detections.append({
-                "box": (int(box[0]), int(box[1]), int(box[2] - box[0]), int(box[3] - box[1])),
-                "confidence": float(conf),
-                "class_id": int(cls)
-            })
-        return detections
+camera = videoSource(
+    '/dev/video0',
+    argv=['--input-width=1280', '--input-height=720']
+    )
+display = videoOutput('display://0')
+while display.isStreaming():
+    img = camera.Capture()
+    detections = net.Detect(img)
+    display.Render(img)
+    display.SetStatus(
+        "Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 
-
-# Example usage:
-if __name__ == "__main__":
-    detector = ObjectDetector()
-    cam = Camera()
-    while True:
-        frame = cam.capture_frame()
-        detections = detector.detect_objects(frame)
-        frame = draw_detections(frame, detections, detector.model.names)
-        cv2.imshow('Object Detection', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cam.close()
-    cv2.destroyAllWindows()
+    for detection in detections:
+        print(detection)
