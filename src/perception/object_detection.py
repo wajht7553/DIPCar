9ce637d2217a51d44d459b3a9d13e94114a9e1b7
wajht7553 <wajahatullah235@gmail.com>
2024-09-03 15:44:56 +0500

    Add object detection

--- src/perception/object_detection.py
+++ src/perception/object_detection.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
-from jetson.inference import detectNet
-from jetson.utils import videoSource, videoOutput
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput
 
 
-net = detectNet(
-    model='../../data/models/ssd-mobilenet.onnx',
-    threshold=0.5
-    )
+class ObjectDetector:
+    def __init__(self, model_path, labels_path,
+                 threshold=0.5, camera=None,
+                 display=False):
+        self.model_path = model_path
+        self.labels_path = labels_path
+        self.threshold = threshold
+        self.camera = videoSource(camera)
+        if display:
+            self.display = videoOutput('display://0')
+        self.net = detectNet(
+            model=self.model_path, labels=self.labels_path,
+            input_blob='input_0', output_cvg='scores',
+            output_bbox='boxes', threshold=self.threshold)
 
-camera = videoSource(
-    '/dev/video0',
-    argv=['--input-width=1280', '--input-height=720']
-    )
-display = videoOutput('display://0')
-while display.isStreaming():
-    img = camera.Capture()
-    detections = net.Detect(img)
-    display.Render(img)
-    display.SetStatus(
-        "Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+    def detect(self):
+        while True:
+            image = self.camera.Capture()
+            detections = self.net.Detect(image)
+            for detection in detections:
+                print(detection)
+            if self.display:
+                self.display.Render(image)
+                self.display.SetStatus(
+                    f"Detection Network {self.net.GetNetworkFPS():.0f} FPS"
+                )
+            if not self.camera.IsStreaming():
+                break
 
-    for detection in detections:
-        print(detection)
+
+def main():
+    model_path = '/home/dipcar/DIPCar/data/models/ssd-mobilenet.onnx'
+    labels_path = '/home/dipcar/DIPCar/data/models/labels.txt'
+    detector = ObjectDetector(model_path, labels_path, display=True)
+    detector.detect()
+
+
+if __name__ == "__main__":
+    main()
