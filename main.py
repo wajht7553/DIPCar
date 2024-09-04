d5c606cacb02b7f53b925491910e73a054154e68
wajht7553 <wajahatullah235@gmail.com>
2024-09-04 11:45:48 +0500

    Add object detection

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-import cv2
-
-from src.perception.utils.camera import Camera
-from src.control.motor_controller import DIPCar
+#!/usr/bin/python3
+from jetson_inference import detectNet
+# from src.control.motor_controller import DIPCar
 from jetson_utils import videoSource, videoOutput
-from src.perception.object_detection import ObjectDetector
 
 
 def main():
-    camera = '/dev/video0'
-    display = True
-    detector = ObjectDetector(
-        model_path='/home/dipcar/DIPCar/data/models/ssd-mobilenet.onnx',
-        labels_path='/home/dipcar/DIPCar/data/models/labels.txt',
-        camera=camera, display=display
+    camera = videoSource('/dev/video0')
+    display = videoOutput('display://0')
+    net = detectNet(
+        model='data/models/ssd-mobilenet.onnx',
+        labels='data/models/labels.txt',
+        input_blob='input_0', output_cvg='scores', output_bbox='boxes',
         )
-    dipcar = DIPCar()
+    # dipcar = DIPCar()
 
-    
-
+    while True:
+        image = camera.Capture()
+        detections = net.Detect(image)
+        display.Render(image)
+        display.SetStatus(
+            f'Detecting Objects at {net.GetNetworkFPS():.0f} FPS'
+        )
+        for detection in detections:
+            print(detection.Area)
+        if not camera.IsStreaming():
+            break
 
 if __name__ == "__main__":
     main()
