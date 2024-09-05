7037f575d38fb8ce6fef192c60a45f7846f6633f
wajht7553 <wajahatullah235@gmail.com>
2024-09-05 18:35:14 +0500

    Add decision module

--- src/control/decision.py
+++ src/control/decision.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-
-
 class DecisionMaker:
+    """
+    Class representing a decision maker for a car.
+    """
+
     def __init__(self, car):
+        """
+        Initializes the Decision class.
+        Attributes:
+            car: The car object.
+            speed: The current speed of the car.
+            safe_speed: The safe speed of the car.
+            labels: A list of labels for object detection.
+            stop_labels: A list of labels indicating a stop.
+            caution_labels: A list of labels indicating caution.
+            speed_limits: A dictionary mapping speed limit labels
+                to their corresponding speeds.
+        """
+
         self.car = car
         self.speed = self.safe_speed = 25
         self.labels = [
@@ -11,7 +26,7 @@ class DecisionMaker:
             'stop', 'traffic_green', 'traffic_red','traffic_yellow'
             ]
         self.stop_labels = ['stop', 'traffic_red']
-        self.cation_labels = ['person', 'bicycle',]
+        self.caution_labels = ['person', 'bicycle',]
         self.speed_limits = {
             'speed_limit_30': 30,
             'speed_limit_60': 60,
@@ -20,9 +35,28 @@ class DecisionMaker:
         self.start()
 
     def start(self):
+        """
+        Starts the decision process for controlling the car.
+        This method is responsible for initiating the decision process
+        for controlling the car. It calls the 'forward' method of the
+        'car' object with the safe speed.
+        Parameters:
+            self: The instance of the 'decision' class.
+        Returns:
+            None
+        """
+
         self.car.forward(self.safe_speed)
 
     def make_decision(self, detections):
+        """
+        Makes a decision based on the given detections.
+        Parameters:
+            detections: A list of detections.
+        Returns:
+            None
+        """
+
         is_cautious = False
         self.car.is_moving = True
         for detection in detections:
@@ -33,7 +67,7 @@ class DecisionMaker:
                 self.car.stop()
                 print('Stop sign detected. Stopping the car.')
                 return
-            elif label in self.cation_labels:
+            elif label in self.caution_labels:
                 is_cautious = True
                 print(f'{label} detected. Slowing down.')
             elif label in self.speed_limits:
