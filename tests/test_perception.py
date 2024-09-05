5d531250249817f8028f2205670016d9f562cc72
wajht7553 <wajahatullah235@gmail.com>
2024-09-05 19:33:36 +0500

    Add unittests for control, perception, and planning

--- tests/test_perception.py
+++ tests/test_perception.py
@@ -1,33 +1,3 @@
 import unittest
-from src.perception.utils.camera import Camera
-from src.perception.object_detection import ObjectDetector
+from jetson_inference import detectNet
 
-
-class TestCamera(unittest.TestCase):
-    def test_camera_initialization(self):
-        camera = Camera()
-        self.assertIsNotNone(camera.cap)
-
-    def test_capture_frame(self):
-        camera = Camera()
-        frame = camera.capture_frame()
-        self.assertEqual(len(frame.shape), 3)
-        camera.close()
-
-
-class TestObjectDetection(unittest.TestCase):
-    def test_object_detection_initialization(self):
-        detector = ObjectDetector("../data/models/yolov5s.pt")
-        self.assertIsNotNone(detector.model)
-
-    def test_detect_objects(self):
-        detector = ObjectDetector("../data/models/yolov5s.pt")
-        camera = Camera()
-        frame = camera.capture_frame()
-        detections = detector.detect_objects(frame)
-        self.assertIsInstance(detections, list)
-        camera.close()
-
-
-if __name__ == "__main__":
-    unittest.main()
