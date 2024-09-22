20a19d38d5f9d8f27945a838bff9d0bf754684fe
wajht7553 <wajahatullah235@gmail.com>
2024-09-22 19:00:56 +0500

    Try adding segmentation

--- enhanced_main.py
+++ enhanced_main.py
@@ -1,80 +1,113 @@
 #!/usr/bin/python3
 import time
-import threading
 from jetson_inference import detectNet, segNet
-from src.control.enhanced_decision import DecisionMaker
+from src.control.decision import DecisionMaker
 from src.control.motor_controller import DIPCar
-from jetson_utils import (videoSource, videoOutput,
-                          cudaAllocMapped, cudaDeviceSynchronize)
-
-
-def run_detection(camera, net, decision_maker, lock):
-    while True:
-        image = camera.Capture()
-        detections = net.Detect(image, overlay='box,labels,lines')
-        with lock:
-            decision_maker.update_detections(detections)
-
-def run_segmentation(camera, seg_net, decision_maker, lock):
-    while True:
-        image = camera.Capture()
-        segmentation = seg_net.Process(image)
-        with lock:
-            decision_maker.update_segmentation(segmentation)
+from jetson_utils import videoSource, videoOutput, cudaOverlay
+from segnet_utils import SegmentationBuffers
+import argparse
 
 
 def main():
     camera = videoSource('/dev/video0')
     display = videoOutput('display://0')
 
-    net = detectNet(
+    # Detection model setup
+    det_net = detectNet(
         model='data/models/detection/ssd-mobilenet.onnx',
         labels='data/models/detection/labels.txt',
         input_blob='input_0', output_cvg='scores',
         output_bbox='boxes', threshold=0.45
-        )
+    )
 
-    segnet = segNet(
+    # Segmentation model setup
+    args = argparse.Namespace(
+        filter_mode='linear',
+        visualize='overlay,mask',
+        ignore_class='void',
+        alpha='150.0',
+        stats=False
+    )
+    seg_net = segNet(
         model="data/models/segmentation/fcn_resnet18.onnx",
         labels="data/models/segmentation/labels.txt",
         input_blob="input_0", output_blob="output_0"
     )
+    seg_net.SetOverlayAlpha(150)
+    buffers = SegmentationBuffers(seg_net, args)
 
     dipcar = DIPCar()
     decision_maker = DecisionMaker(dipcar)
 
-    lock = threading.Lock()
-
-    det_thread = threading.Thread(
-        target=run_detection,
-        args=(camera, net, decision_maker, lock)
-    )
-    seg_thread = threading.Thread(
-        target=run_segmentation,
-        args=(camera, segnet, decision_maker, lock)
-    )
-    det_thread.start()
-    seg_thread.start()
-
     try:
         while True:
+            # Capture image
             image = camera.Capture()
-            decision_maker.make_decision()
-            display.Render(image)
+
+            # Run detection
+            detections = det_net.Detect(image, overlay='box,labels,lines')
+
+            # Run segmentation
+            buffers.Alloc(image.shape, image.format)
+            seg_net.Process(image, ignore_class=args.ignore_class)
+
+            if buffers.overlay:
+                seg_net.Overlay(buffers.overlay, filter_mode=args.filter_mode)
+
+            if buffers.mask:
+                seg_net.Mask(buffers.mask, filter_mode=args.filter_mode)
+
+            # Simple steering control based on segmentation mask
+            # road_center = analyze_road_center(buffers.mask)
+            # steer_car(dipcar, road_center)
+
+            # Make decision based on detections
+            decision_maker.make_decision(detections)
+
+            # Render output
+            if buffers.composite:
+                cudaOverlay(buffers.overlay, buffers.composite, 0, 0)
+                cudaOverlay(buffers.mask, buffers.composite,
+                            buffers.overlay.width, 0)
+                display.Render(buffers.output)
+            else:
+                display.Render(image)
+
             display.SetStatus(
-                f'Detecting Objects at {net.GetNetworkFPS():.0f} FPS'
-                )
-            end_time = time.time()
+                f'Detection: {det_net.GetNetworkFPS():.0f} FPS, Segmentation: {seg_net.GetNetworkFPS():.0f} FPS'
+            )
+
             if not camera.IsStreaming():
                 print("Camera failure!!!")
                 decision_maker.close()
                 break
-    except KeyboardInterrupt:
-        dipcar.stop()
-    finally:
+
         dipcar.cleanup()
-        det_thread.join()
-        seg_thread.join()
+    except KeyboardInterrupt:
+        dipcar.cleanup()
+
+
+def analyze_road_center(mask):
+    # Simple analysis: check the bottom half of the image
+    height, width = mask.shape
+    bottom_half = mask[height//2:, :]
+    left_sum = bottom_half[:, :width//2].sum()
+    right_sum = bottom_half[:, width//2:].sum()
+
+    if left_sum > right_sum:
+        return 'left'
+    elif right_sum > left_sum:
+        return 'right'
+    else:
+        return 'center'
+
+
+def steer_car(dipcar, road_center):
+    if road_center == 'left':
+        dipcar.steer_left(25)
+    elif road_center == 'right':
+        dipcar.steer_right(25)
+    # If center, the decision_maker will handle forward motion
 
 
 if __name__ == "__main__":
