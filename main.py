b1370dfcaef3e9be1ad3fff1c3d8229ff81d4f9a
wajht7553 <wajahatullah235@gmail.com>
2024-09-22 17:05:55 +0500

    Add segmentation

--- main.py
+++ main.py
@@ -11,28 +11,32 @@ def main():
     display = videoOutput('display://0')
     net = detectNet(
         model='data/models/detection/ssd-mobilenet.onnx',
-        labels='data/models/segmentation/labels.txt',
+        labels='data/models/detection/labels.txt',
         input_blob='input_0', output_cvg='scores',
         output_bbox='boxes', threshold=0.45
         )
     time.sleep(5)
     dipcar = DIPCar()
     decision_maker = DecisionMaker(dipcar)
-
-    while True:
-        image = camera.Capture()
-        detections = net.Detect(image, overlay='box,labels,lines')
-        decision_maker.make_decision(detections)
-        display.Render(image)
-        display.SetStatus(
-            f'Detecting Objects at {net.GetNetworkFPS():.0f} FPS'
-            )
-        if not camera.IsStreaming():
-            print("Camera failure!!!")
-            decision_maker.close()
-            break
-
-    dipcar.cleanup()
+    try:
+        while True:
+            start_time = time.time()
+            image = camera.Capture()
+            detections = net.Detect(image, overlay='box,labels,lines')
+            decision_maker.make_decision(detections)
+            display.Render(image)
+            display.SetStatus(
+                f'Detecting Objects at {net.GetNetworkFPS():.0f} FPS'
+                )
+            end_time = time.time()
+            print(f"latency: {end_time-start_time}")
+            if not camera.IsStreaming():
+                print("Camera failure!!!")
+                decision_maker.close()
+                break
+        dipcar.cleanup()
+    except KeyboardInterrupt:
+        dipcar.cleanup()
 
 
 if __name__ == "__main__":
--- src/control/decision.py
+++ src/control/decision.py
@@ -19,18 +19,18 @@ class DecisionMaker:
         """
 
         self.car = car
-        self.speed = self.safe_speed = 25
+        self.speed = self.safe_speed = 15
         self.labels = [
-            'BACKGROUND', 'bicycle', 'person', 
+            'BACKGROUND', 'bicycle', 'person',
             'speed_limit_120', 'speed_limit_30', 'speed_limit_60',
             'stop', 'traffic_green', 'traffic_red', 'vehicle'
             ]
         self.stop_labels = ['stop', 'traffic_red']
-        self.caution_labels = ['person', 'bicycle',]
+        self.caution_labels = ['person', 'bicycle']
         self.speed_limits = {
-            'speed_limit_30': 30,
-            'speed_limit_60': 60,
-            'speed_limit_120': 100,
+            'speed_limit_30': 25,
+            'speed_limit_60': 50,
+            'speed_limit_120': 70,
         }
         self.start()
 
