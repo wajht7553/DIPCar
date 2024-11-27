396c226b75422abae5799ea24334d3d00574db47
Wajahat Ullah <wajahatullah235@gmail.com>
2024-11-27 20:36:12 +0500

    Test code

--- enhanced_main.py
+++ enhanced_main.py
@@ -6,7 +6,7 @@ from jetson_inference import detectNet, segNet
 from src.control.decision import DecisionMaker
 from src.control.motor_controller import DIPCar
 from jetson_utils import videoSource, videoOutput, cudaOverlay, cudaToNumpy
-import cv2
+# import cv2
 
 
 def main():
